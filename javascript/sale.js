8cee9c7aa273c2de3f40d746dced3a11d612f235
sohan0898 <shuhanur.rahman08@gmail.com>
2023-08-21 15:58:58 +0600

    added all section make dynamic and responsive added

--- javascript/sale.js
+++ javascript/sale.js
@@ -1,12 +1,33 @@
 let total = 0;
 
+function handleClickCard(target){
+
+    const selectedItemContainer = document.getElementById("selected-items");
+    
+    const itemName = target.childNodes[3].childNodes[5].innerText;
+    const li = document.createElement("li");
+    li.innerText = itemName;
+    selectedItemContainer.appendChild(li);
+
+    const price = target.childNodes[3].childNodes[7].innerText.split(" ")[0];
+
+    total = parseFloat(total) + parseFloat(price);
+
+    document.getElementById("total-price").innerText = total;
+    document.getElementById("total-after-discount").innerText = total;
+    
+
+    // updatebutton 
+    updateButtonStatus();
+
+}
 
 function applyCoupon() {
     const couponCode = document.getElementById("couponCode").value;
 
     if (couponCode === "SELL200") {
-        // Apply 20% discount
-        const discountAmount = total * 0.2; // 20% discount
+        // 20% discount
+        const discountAmount = total * 0.2; 
         const discountedTotal = total - discountAmount;
         
         // Update discount and total prices
@@ -16,34 +37,7 @@ function applyCoupon() {
         // Clear the input field
         document.getElementById("couponCode").value = "";
 
-        // Update button status
-        updateButtonStatus();
     }
-}
-
-function handleClickCard(target){
-
-    const selectedItemContainer = document.getElementById("selected-items");
-    
-    const itemName = target.parentNode.parentNode.childNodes[3].childNodes[5].innerText;
-    const li = document.createElement("li");
-    li.innerText = itemName;
-    selectedItemContainer.appendChild(li);
-
-    const price = target.parentNode.parentNode.childNodes[3].childNodes[7].innerText.split(" ")[0];
-
-    total = parseFloat(total) + parseFloat(price);
-
-    document.getElementById("total-price").innerText = total;
-
-    updateButtonStatus();
-
-    // // Check if total price is 200 TK or more
-    // if (total >= 200) {
-    //     document.getElementById("applyButton").removeAttribute("disabled");
-    // } else {
-    //     document.getElementById("applyButton").setAttribute("disabled", "true");
-    // }
 }
 
 function updateButtonStatus() {
@@ -64,18 +58,20 @@ function updateButtonStatus() {
 }
 
 function goHome() {
-    // Clear the selected items list
+
     const selectedItemContainer = document.getElementById("selected-items");
     selectedItemContainer.innerHTML = "";
 
-    // Reset the total price and discounts
+    // Reset the cart or coupon
     total = 0;
     document.getElementById("total-price").innerText = total.toFixed(2);
     document.getElementById("discount-price").innerText = "00.00";
     document.getElementById("total-after-discount").innerText = "00.00";
 
-    // Disable the Apply button
+    // Disable Apply and Make Purchase Button
     document.getElementById("applyButton").setAttribute("disabled", "true");
+    document.getElementById("purchaseButton").setAttribute("disabled", "true");
+
 
     // Close the modal
     const modal = document.getElementById("my_modal_1");
